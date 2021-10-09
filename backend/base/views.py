e73db263194d6e688e71a14de1e519895388784c
d.tuytuyshkin@gmail.com <d.tuytuyshkin@gmail.com>
2021-10-09 17:56:39 +0300

    Added first routes endpoint and requirements for project

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(["GET"])
+def get_routes(request):
+    routes = [
+        "/api/products/",
+        "/api/products/create/",
+        "/api/products/upload/",
+        "/api/products/<id>/reviews/",
+        "/api/products/top/",
+        "/api/products/<id>/",
+        "/api/products/delete/<id>/",
+        "/api/products/update/<id>/",
+    ]
+    return Response(routes)
