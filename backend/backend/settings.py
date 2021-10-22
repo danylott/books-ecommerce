dba2bc617404cff7d4f8f4caf33b5517cc34e661
d.tuytuyshkin@gmail.com <d.tuytuyshkin@gmail.com>
2021-10-22 22:10:05 +0300

    Added postgresql free database. Use raw password - because free version - not important.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,7 +78,14 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "zpbfqvft",
+        "USER": "zpbfqvft",
+        "PASSWORD": "Ia6cNznKI3C7v-GiQyQbVyx3T76k3GIA",
+        "HOST": "hattie.db.elephantsql.com",
+        "PORT": "5432",
+    }
 }
 
 
