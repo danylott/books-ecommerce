8777caf38f777d7ef2c3e78b7a5485a912a5f4a5
d.tuytuyshkin@gmail.com <d.tuytuyshkin@gmail.com>
2021-10-22 21:57:15 +0300

    Renamed products -> real_estate. Added basic endpoints structure.

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.get_routes, name="routes"),
+    path("real_estates/", views.get_real_estates, name="real_estates"),
+    path("real_estate/<str:pk>", views.get_real_estate, name="real_estate"),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -6,13 +5,23 @@ from rest_framework.response import Response
 @api_view(["GET"])
 def get_routes(request):
     routes = [
-        "/api/products/",
-        "/api/products/create/",
-        "/api/products/upload/",
-        "/api/products/<id>/reviews/",
-        "/api/products/top/",
-        "/api/products/<id>/",
-        "/api/products/delete/<id>/",
-        "/api/products/update/<id>/",
+        "/api/real_estate/",
+        "/api/real_estate/create/",
+        "/api/real_estate/upload/",
+        "/api/real_estate/<id>/reviews/",
+        "/api/real_estate/top/",
+        "/api/real_estate/<id>/",
+        "/api/real_estate/delete/<id>/",
+        "/api/real_estate/update/<id>/",
     ]
     return Response(routes)
+
+
+@api_view(["GET"])
+def get_real_estates(request):
+    return Response()
+
+
+@api_view(["GET"])
+def get_real_estate(request, pk):
+    return Response()
