c27a01297ffd20d2b5ca267da28f526efbfc2edd
vase444ka <vborysyuk@live.com>
2021-11-16 01:13:43 +0200

    implemented redux state management for product list on home screen and product details

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
+
+import { useDispatch, useSelector } from 'react-redux'
 
 import products from '../products'
 import Product from '../components/Product'
+import { listProducts } from '../actions/productActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 function HomeScreen() {
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const { error, loading, products, page, pages } = productList
+
+    useEffect(() => {
+        dispatch(listProducts())
+    }, [dispatch])
+
     return (
         <div>
             <h1>
                 Popular Books
             </h1>
-            <Row>
-                {products.map(product => (
-                    <Col  xs={9} sm={7} md={5} lg={3} xl={2} key={product._id}>
-                        <Product product={product}/>
-                    </Col>
-                ))}
-            </Row>
+            {loading ? <Loader />
+                : error ? <Message variant='danger'>{error}</Message>
+                    :
+                    <Row>
+                        {products.map(product => (
+                            <Col xs={9} sm={7} md={5} lg={3} xl={2} key={product._id}>
+                                <Product product={product} />
+                            </Col>
+                        ))}
+                    </Row>
+            }
         </div>
     )
 }
