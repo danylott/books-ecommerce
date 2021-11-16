43fc3e84584da4ff0af676e54e1c8c895c42957b
d.tuytuyshkin@gmail.com <d.tuytuyshkin@gmail.com>
2021-11-16 21:32:52 +0200

    Added user endpoints and authentication

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
-    path("users/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("", views.get_routes, name="routes"),
+
+    path("users/login/", views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
+
+    path("users/profile/", views.get_user_profile, name="users-profile"),
+    path("users/", views.get_users, name="users"),
+
     path("products/", views.get_products, name="products"),
     path("products/<str:pk>/", views.get_product, name="product"),
 ]
