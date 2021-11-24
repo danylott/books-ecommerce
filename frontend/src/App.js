c66977a3a10f0e00ab74ff65fc520a1bb8d7b490
Vlad Snegovsky <nik.snegovskyvlad@gmail.com>
2021-11-24 09:50:51 +0200

    shipping and place order front vs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import { Container } from 'react-bootstrap'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import Footer from './components/Footer.js';
-import Header from './components/Header.js';
-import HomeScreen from './screens/HomeScreen.js';
-import ProductScreen from './screens/ProductScreen.js';
-import CartScreen from './screens/CartScreen.js';
-import LoginScreen from './screens/LoginScreen.js';
-import RegisterScreen from './screens/RegisterScreen.js';
-import ProfileScreen from './screens/ProfileScreen.js';
+import Footer from './components/Footer.js'
+import Header from './components/Header.js'
+import HomeScreen from './screens/HomeScreen.js'
+import ProductScreen from './screens/ProductScreen.js'
+import CartScreen from './screens/CartScreen.js'
+import LoginScreen from './screens/LoginScreen.js'
+import RegisterScreen from './screens/RegisterScreen.js'
+import ProfileScreen from './screens/ProfileScreen.js'
+import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 function App() {
   return (
@@ -21,6 +24,9 @@ function App() {
             <Route path='/login' component={LoginScreen} />
             <Route path='/register' component={RegisterScreen} />
             <Route path='/profile' component={ProfileScreen} />
+            <Route path='/shipping' component={ShippingScreen} />
+            <Route path='/placeorder' component={PlaceOrderScreen} />
+            <Route path='/payment' component={PaymentScreen} />
             <Route path='/product/:id' component={ProductScreen} />
             <Route path='/cart/:id?' component={CartScreen} />
           </Switch>
