50b7efdb64daf41cc36ee38ef9a603937509ae59
IvanRamyk <ivan.r@mate.academy>
2021-11-28 21:31:14 +0200

    Backend: fix imports

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from base.models import Product
-from ..serializers import ProductSerializer
+from base.serializers import ProductSerializer
 
 
 @api_view(["GET"])
