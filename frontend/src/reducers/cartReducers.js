a4921b8cdbed660e68904b88889f981d7c0adc60
VladSnegovsky <50777219+VladSnegovsky@users.noreply.github.com>
2021-11-30 18:56:08 +0200

    Update cartReducers.js

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,10 +1,11 @@
-import { 
+import {
     CART_ADD_ITEM,
-    CART_REMOVE_ITEM, 
+    CART_REMOVE_ITEM,
     CART_SAVE_SHIPPING_ADDRESS,
+
     CART_SAVE_PAYMENT_METHOD,
 
-    CART_CLEAR_ITEMS
+    CART_CLEAR_ITEMS,
 } from '../constants/cartConstants'
 
 
